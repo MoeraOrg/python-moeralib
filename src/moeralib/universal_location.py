d7b6eb14687cd5c5c5ae9bfbb27b168d83fa5a39
Shmuel Melamud <smelamud@redhat.com>
2024-03-17 05:18:36 +0200

    Incorrect module reference.

--- src/moeralib/universal_location.py
+++ src/moeralib/universal_location.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from urllib.parse import urlparse, urlunparse
 
-from src.moeralib.naming.naming import shorten
+from .naming.naming import shorten
 
 REDIRECTOR = 'moera.page'
 
