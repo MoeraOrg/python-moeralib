9048cebc59063320d2bcf12c81c7e2662a6dbf01
Shmuel Melamud <smelamud@redhat.com>
2024-06-14 02:51:44 +0300

    Missing exports.

--- src/moeralib/naming/__init__.py
+++ src/moeralib/naming/__init__.py
@@ -1,2 +1,2 @@
 from .naming import (MoeraNaming, MoeraNamingError, MoeraNamingConnectionError, MAIN_SERVER, DEV_SERVER,
-                     node_name_parse, resolve)
+                     node_name_parse, expand, resolve, shorten)
