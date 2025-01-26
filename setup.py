545311b9e04312baa4aac26a4ecc4e888faf5efd
Shmuel Melamud <smelamud@redhat.com>
2025-01-26 04:48:21 +0200

    Updated dependencies due to vulnerabilities.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='moeralib',
     version='0.16.0',
     install_requires=[
-        'requests>=2.31.0',
+        "requests>=2.32.0",
         'camel-converter',
         'jsonschema>=4.19.0',
-        'cryptography~=42.0.4',
+        "cryptography~=43.0.1",
         'mnemonic',
         'ecpy',
     ],
