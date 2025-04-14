a8aeaf3e90e4dd443d3da46ec5cea6bea8484c3b
Shmuel Melamud <smelamud@redhat.com>
2025-04-15 00:06:57 +0300

    Release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='moeralib',
-    version='0.16.0',
+    version='0.16.5',
     install_requires=[
         "requests>=2.32.0",
         'camel-converter',
